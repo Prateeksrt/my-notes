f888131810e0c4d60b7bf73ead27af1f657d800a
Prateek Srivastava <prateeksrt2009@gmail.com>
2020-10-13 21:56:21 +0700

    Created the NoteList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import CreateNote from "./Component/CreateNote/CreateNote";
+import NoteList from "./Component/NoteList/NoteList";
+
+const Notes = [
+    {title: "First Note", body: "", selected: true},
+    {title: "Second Note", body: "", selected: false},
+    {title: "Third Note", body: "", selected: false},
+    {title: "Fourth Note", body: "", selected: false},
+    {title: "Fifth Note", body: "", selected: false},
+    {title: "Sixth Note", body: "", selected: false},
+    {title: "Last Note", body: "", selected: false},
+];
 
 const App = () => {
     return (
         <div>
-            <CreateNote />
+            <NoteList noteList={Notes}/>
         </div>
     );
 };
---
+++ src/Component/NoteList/NoteList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NoteList.css';
+
+const NoteList = () => {
+    return (
+        <div className="NoteListContainer">
+            <div className="NoteItem">First Item</div>
+            <div className="NoteItem">Second Item</div>
+        </div>
+    );
+};
+
+export default NoteList;
